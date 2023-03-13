417ffac7ec3cf5431fc43ed42e866fe7cf9d21fc
Mark <94270731+HexerMaster1929@users.noreply.github.com>
2023-03-13 01:33:38 -0400

    Update moduleloader.lua

--- modules/moduleloader.lua
+++ modules/moduleloader.lua
@@ -1,10 +1,16 @@
-TabsManager = require(TestingModules.TabsManager)
-ExecutorManager = require(TestingModules.ExecutorManager)
-BackdoorModule = require(TestingModules.BackdoorManager)
-ListManager = require(TestingModules.ListManager)
-TextTipManager = require(TestingModules.TextTipManager)
-MessageBox = require(TestingModules.MessageBoxManager)
-EditorFunctions = require(TestingModules.HexSSEditorFunctions)
+local Branch = "Stable"-- Latest,Stable
+
+local function import(file)
+	return loadstring(game:HttpGet(string.format("https://raw.githubusercontent.com/HexerMaster1929/HexSS/%s/modules/%s.lua",Branch,file)))()
+end
+
+TabsManager = import("TabsManager")
+ExecutorManager = import("ExecutorManager")
+BackdoorModule = import("BackdoorManager")
+ListManager = import("ListManager")
+TextTipManager = import("TextTipManager")
+MessageBox = import("MessageBoxManager")
+EditorFunctions = import("HexSSEditorFunctions")
 
 --[[TabsManager = require(TestingModules.TabsManager)
 ExecutorManager = require(TestingModules.ExecutorManager)
